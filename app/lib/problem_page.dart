7af657252eebf612ab9ca1e48a6e5264d4b214e4
Guruprasad Kulkarni <guru@linux.com>
2023-09-30 20:12:53 +0200

    Added Header

--- app/lib/problem_page.dart
+++ app/lib/problem_page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:simple_math_tester/main.dart';
@@ -60,54 +62,65 @@ class _BigCardState extends State<BigCard> {
     final appState = context.watch<MathTesterModel>();
     var theme = Theme.of(context);
     var style = theme.textTheme.displayMedium!.copyWith(color: theme.colorScheme.onPrimary);
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: SizedBox(
-          width: 300,
-          height: 300,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                children: [
-                  Padding(
-                      padding: const EdgeInsets.only(left: 30.0), child: Text(appState.current.left, style: style, semanticsLabel: "First Operand is ${appState.current.left}")),
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 5.0),
-                    child: Text(appState.current.operationType.displayString, style: style, semanticsLabel: "Operation is ${appState.current.operationType.displayString}"),
-                  ),
-                  Text(appState.current.right, style: style, semanticsLabel: "Second Operand is ${appState.current.right}"),
-                ],
-              ),
-              TextField(
-                keyboardType: const TextInputType.numberWithOptions(signed: true, decimal: true),
-                style: style,
-                controller: userInputController,
-                onTapOutside: (_) {
-                  final userInput = double.tryParse(userInputController.value.text);
-                  debugPrint('changed value : $userInput');
-                  appState.setInput(userInput);
-                  userInputController.clear();
-                },
-                onSubmitted: (value) {
-                  final userInput = double.tryParse(value);
-                  debugPrint('value : $userInput');
-                  appState.checkInput();
-                  userInputController.clear();
-                },
-              ),
-            ],
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Text(
+            'Simple Math Questions.',
+            style: theme.textTheme.displayMedium!.copyWith(color: theme.colorScheme.inversePrimary),
           ),
         ),
-      ),
+        Card(
+          color: theme.colorScheme.primary,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: SizedBox(
+              width: 300,
+              height: 300,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                          padding: const EdgeInsets.only(left: 30.0), child: Text(appState.current.left, style: style, semanticsLabel: "First Operand is ${appState.current.left}")),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 5.0),
+                        child: Text(appState.current.operationType.displayString, style: style, semanticsLabel: "Operation is ${appState.current.operationType.displayString}"),
+                      ),
+                      Text(appState.current.right, style: style, semanticsLabel: "Second Operand is ${appState.current.right}"),
+                    ],
+                  ),
+                  TextField(
+                    keyboardType: const TextInputType.numberWithOptions(signed: true, decimal: true),
+                    style: style,
+                    controller: userInputController,
+                    onTapOutside: (_) {
+                      final userInput = double.tryParse(userInputController.value.text);
+                      debugPrint('changed value : $userInput');
+                      appState.setInput(userInput);
+                      userInputController.clear();
+                    },
+                    onSubmitted: (value) {
+                      final userInput = double.tryParse(value);
+                      debugPrint('value : $userInput');
+                      appState.checkInput();
+                      userInputController.clear();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
